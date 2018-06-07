36512b52b6c856ee418142d0b9219e203384bb65
Naja Kamil <NAJAK@amdocs.com>
2018-06-07 19:13:54 +0200

    added new methods

--- src/Converter.java
+++ src/Converter.java
@@ -2,25 +2,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Converter implements FileConverter {
-
-	public String readFile(Path input) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(input.toString()));
-		String line;
-		String readedText = "";
-		while((line = reader.readLine()) != null)
-			readedText += line;
-		reader.close();
-		return readedText;
-	}
 
 	@Override
 	public void convertFile(Path input, Path output) {
 		try {
-			System.out.println(readFile(input));
+			readFile(input);
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 	}
@@ -29,5 +24,33 @@ public class Converter implements FileConverter {
 	public void convertFile(File input, File output) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public String hashTextLine(String line) throws NoSuchAlgorithmException {
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.update(line.getBytes(), 0, line.length());
+		System.out.println("MD5 " + new BigInteger(1, m.digest()).toString(16));
+		String hashedData = new BigInteger(1, m.digest()).toString(16);
+		return hashedData;
+	}
+
+	public void saveHashedFile(String hashedText) {
+		System.out.println(hashedText);
+	}
+
+	public String readFile(Path input) throws IOException, NoSuchAlgorithmException {
+		BufferedReader reader = new BufferedReader(new FileReader(input.toString()));
+		String line;
+		String readedText = "";
+		while ((line = reader.readLine()) != null) {
+			if (line.length() > 0) {
+				readedText += (line);
+//				zapisz zahashowane dane
+				saveHashedFile(hashTextLine(line));
+//				System.out.println(line);
+			}
+		}
+		reader.close();
+		return readedText;
 	}
 }
