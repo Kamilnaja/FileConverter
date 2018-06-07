784616155d0ae2bcea247d7b75abb656ded2a3d4
Naja Kamil <NAJAK@amdocs.com>
2018-06-07 19:51:49 +0200

    finished implementation

--- src/Converter.java
+++ src/Converter.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.nio.file.Path;
 import java.security.MessageDigest;
@@ -9,10 +11,15 @@ import java.security.NoSuchAlgorithmException;
 
 public class Converter implements FileConverter {
 
+	private Path output;
+	private Path input;
+
 	@Override
 	public void convertFile(Path input, Path output) {
+		this.input = input;
+		this.output = output;
 		try {
-			readFile(input);
+			readFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
@@ -29,27 +36,28 @@ public class Converter implements FileConverter {
 	public String hashTextLine(String line) throws NoSuchAlgorithmException {
 		MessageDigest m = MessageDigest.getInstance("MD5");
 		m.update(line.getBytes(), 0, line.length());
-		System.out.println("MD5 " + new BigInteger(1, m.digest()).toString(16));
 		String hashedData = new BigInteger(1, m.digest()).toString(16);
 		return hashedData;
 	}
 
-	public void saveHashedFile(String hashedText) {
-		System.out.println(hashedText);
+	public void saveHashedText(String hashedText) throws FileNotFoundException {
+		System.out.println("text to save = " + hashedText);
+		PrintWriter out = new PrintWriter(output.toString());
+		out.println(hashedText);
+		out.close();
 	}
 
-	public String readFile(Path input) throws IOException, NoSuchAlgorithmException {
+	public String readFile() throws IOException, NoSuchAlgorithmException {
 		BufferedReader reader = new BufferedReader(new FileReader(input.toString()));
 		String line;
 		String readedText = "";
+		String textToSave = "";
 		while ((line = reader.readLine()) != null) {
 			if (line.length() > 0) {
-				readedText += (line);
-//				zapisz zahashowane dane
-				saveHashedFile(hashTextLine(line));
-//				System.out.println(line);
+				textToSave += (line + "; " + hashTextLine(line) + "\r");
 			}
 		}
+		saveHashedText(textToSave);
 		reader.close();
 		return readedText;
 	}
